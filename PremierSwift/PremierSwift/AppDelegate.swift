3087d2192954ce9efdcf72eaaefd306132ecbde1
Stefan Dougan-Hyde <stefan@deliveroo.co.uk>
2016-09-27 15:53:45 +0100

    Create Swift app.

---
+++ PremierSwift/PremierSwift/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let moviesViewController = MoviesViewController()
+        let navigationController = UINavigationController(rootViewController: moviesViewController)
+        window!.rootViewController = navigationController
+        window!.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ PremierSwift/PremierSwift/NSAttributedString+Premier.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+extension NSAttributedString {
+    
+    var titleAttributes: [String: Any] {
+        return [NSFontAttributeName: UIFont.preferredFont(forTextStyle: UIFontTextStyle.title1), NSForegroundColorAttributeName: UIColor.darkGray]
+    }
+    
+    var bodyAttributes: [String: Any] {
+        return [NSFontAttributeName: UIFont.preferredFont(forTextStyle: UIFontTextStyle.body), NSForegroundColorAttributeName: UIColor.gray]
+    }
+    
+}
+
